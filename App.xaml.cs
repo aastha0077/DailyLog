fee94aa15bc507d93c3eb1e27d266612e6280355
aastha0077 <aryalaastha62@gmail.com>
2025-12-21 17:44:15 +0545

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyJournals;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyJournals;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
